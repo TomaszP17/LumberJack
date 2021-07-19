f9a70949060db911c3bf394b9d5563b8fd59b48c
TomaszP17 <70948711+TomaszP17@users.noreply.github.com>
2021-07-19 17:51:34 +0200

    PodazanieAIZaGraczemNaWyznaczonymPolu

--- ProjektUnreal/Intermediate/Build/Win64/UE4Editor/Inc/ProjektUnreal/ProjektUnreal.init.gen.cpp
+++ ProjektUnreal/Intermediate/Build/Win64/UE4Editor/Inc/ProjektUnreal/ProjektUnreal.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeProjektUnreal_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x23A986C1,
-				0x79EB29C8,
+				0x69D249C4,
+				0x2E71D60D,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
---
+++ ProjektUnreal/Source/ProjektUnreal/ShooterAIController.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterAIController.h"
+#include "Kismet/GameplayStatics.h"
+void AShooterAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+
+	
+}
+
+void AShooterAIController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	MoveToActor(PlayerPawn, 200);
+	SetFocus(PlayerPawn);
+}
